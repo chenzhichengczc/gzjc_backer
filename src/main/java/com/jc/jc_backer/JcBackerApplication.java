e1ce00d2a21aeca38d5f831f34f3c5ce5c6339f4
chenzhichengczc <178642170@qq.com>
2019-03-13 11:50:31 +0800

    后台搭建

---
+++ src/main/java/com/jc/jc_backer/JcBackerApplication.java
@@ -0,0 +1,13 @@
+package com.jc.jc_backer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcBackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcBackerApplication.class, args);
+    }
+
+}
